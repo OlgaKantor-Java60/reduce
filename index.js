f8350056818ce4d99ca3a7b86b4436a145b09fdb
OlgaKantor <kntrolga@gmail.com>
2025-02-25 00:37:34 +0200

    hw done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function myReduce(array, callback, initialValue) {
   //first parametr is an array for reducing
   //returns one value after reducing
-  //see mdn web docs
-  let index = initialValue ? 1 : 0;
-  let acc = initialValue ? initialValue : array[0]; //TODO doesn't work with initialValue = 0; or undefined
+  //see mdn web
+  let index = initialValue == undefined ? 1 : 0;
+  let acc = initialValue == undefined ? array[0] : initialValue; 
   for (; index < array.length; index++) {
     if (array[index] != undefined) {
       acc = callback(acc, array[index], index, array);
@@ -13,19 +13,18 @@ function myReduce(array, callback, initialValue) {
 }
 
 function minMaxValue(array) {
-  // TODO strings
   //arr is an array containing either strings or numbers
   //returns array with 2 elements: first - min value, second - max value
   //requirement: to use myReduce method described above
- const res = myReduce(array, (acc, curr) => {
-      acc[0] = acc[0]>curr ? acc[0] = curr : acc[0];
-      acc[1] = acc[1]<curr ? acc[1] = curr : acc[1];
-      return acc;
-  }, [array[0], array[0]])
-return res;
+  const res = myReduce(array, (acc, curr) => {
+    acc[0] = acc[0] > curr ? (acc[0] = curr) : acc[0];
+    acc[1] = acc[1] < curr ? (acc[1] = curr) : acc[1];
+    return acc;
+  }, [array[0], array[0]]);
+  return res;
 }
 
-let arr = [1, 0, 2000, -10, 155];
-console.log(minMaxValue(arr));
- 
-
+let arr1 = [155, 2000, 0, -10, 1];
+let arr2 = ["kukareku", "TelRan", "str", "60", "JavaScript"];
+console.log(`Call of function minMaxValue for array of numbers [${arr1}] => result: ${minMaxValue(arr1)}`);
+console.log(`Call of function minMaxValue for array of strings [${arr2}] => result: ${minMaxValue(arr2)}`);
